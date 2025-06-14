636158cd305e5ec3f603e78ebb797f5f77ae825a
Parky5233 <bdog5233@gmail.com>
2025-06-14 18:05:35 -0400

    Modified code to perform analysis across varying values for C and write to txt file.

--- SVM.py
+++ SVM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import svm
+import warnings
 from collections import Counter, defaultdict
 from sklearn.metrics import classification_report
 from sklearn.metrics import balanced_accuracy_score
@@ -26,7 +27,7 @@ random seeds and report:
 - Precomputed kernel
 '''
 
-def experiment(train_kernel, train_labels, test_kernel, test_labels, top_n, seed=42):
+def experiment(train_kernel, train_labels, test_kernel, test_labels, top_n, seed=42, C=None):
     if top_n is not None:
         class_counts = Counter(train_labels)
         top_classes = set([cls for cls, _ in class_counts.most_common(top_n)])
@@ -39,8 +40,10 @@ def experiment(train_kernel, train_labels, test_kernel, test_labels, top_n, seed
 
         test_kernel = test_kernel[test_idx,:][:, train_idx]
         test_labels = [test_labels[i] for i in test_idx]
-
-    classifier = svm.SVC(kernel='precomputed', class_weight='balanced',random_state=seed)
+    #Papers to read:
+    #A unified approach to interpreting model predictions - shap
+    #Grad-Cam: Visual Explanations from Deep Networks via Gradiant-based localization
+    classifier = svm.SVC(kernel='precomputed', class_weight='balanced',random_state=seed, C=C)#10, 5, 1, 0.5, 0.1 [10,1] [0.9, 0.8, ... 0.1], find best C value and store other results
     classifier.fit(train_kernel,train_labels)
 
     y_pred = classifier.predict(test_kernel)
@@ -63,6 +66,7 @@ def load_labels(tf):
     return label_map
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
     training_kernel, training_ids = load_kernels('data/training_kernel_matrix.txt')
     training_labels = load_labels('data/training_labels.txt')
     y_train = [training_labels[sample_id] for sample_id in training_ids]
@@ -73,17 +77,24 @@ if __name__ == "__main__":
     y_test = [test_labels[sample_id] for sample_id in t2t_ids]
 
     seeds = [i for i in range(20)]
+    Cs = [i for i in range(1,11)]+[j/10 for j in range(1,10,1)]
     reports = defaultdict(list)
     results = defaultdict(list)
-
-    for top_n in [5, 10, None]:
-        for seed in seeds:#20 random seeds
-            report, acc = experiment(training_kernel, y_train, t2t_kernel_matrix, y_test, top_n=top_n, seed=seed)
-            reports[str(top_n)].append(report)
-            results[str(top_n)].append(acc)
-    for key in results:
-        accs = results[key]
-        mean_acc = np.mean(accs)
-        std_acc = np.std(accs)
-        label = f"Top {key} largest classes" if key != "None" else "All Classes"
-        print(f"{label} - Mean Balanced Accuracy: {mean_acc:.4f} ± {std_acc:.4f}")
+    max_acc = 0
+    best_C = None
+    with open('performance_analysis_Cs.txt', 'w', encoding='utf-8') as f:
+        for C in Cs:
+            f.write(f'C = {C}\n')
+            for top_n in [5, 10, None]:
+                for seed in seeds:#20 random seeds
+                    report, acc = experiment(training_kernel, y_train, t2t_kernel_matrix, y_test, top_n=top_n, seed=seed, C=C)
+                    # print(report)
+                    reports[str(top_n)].append(report)
+                    results[str(top_n)].append(acc)
+            for key in results:
+                accs = results[key]
+                mean_acc = np.mean(accs)
+                std_acc = np.std(accs)
+                label = f"Top {key} largest classes" if key != "None" else "All Classes"
+                f.write(f"{label} - Mean Balanced Accuracy: {mean_acc:.4f} \u00B1 {std_acc:.4f}\n")
+            f.write("-------------\n")
